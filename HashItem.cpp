29c34677ef7a51ec765495f3fdf3832842545218
labccet <labccet>
2016-06-01 19:53:30 -0300

    BLABLA

---
+++ HashItem.cpp
@@ -0,0 +1,16 @@
+#include "HashItem.h"
+
+
+      HashItem::HashItem(int chave, string info) {
+            this->chave = chave;
+            this->info = info;
+      }
+ 
+      int HashItem::getChave() {
+            return chave;
+      }
+ 
+      string HashItem::getInfo() {
+            return info;
+      }
+
